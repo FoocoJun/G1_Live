cf7672bb955be535818cd8cccd9ca66ed4f2d696
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2024-04-26 22:30:38 +0900

    create some Managers and apply it to Manager Managers

--- G1/Assets/@Scripts/Managers/Managers.cs
+++ G1/Assets/@Scripts/Managers/Managers.cs
@@ -7,6 +7,23 @@ public class Managers : MonoBehaviour
     private static Managers s_instance;
     private static Managers Instance { get { Init(); return s_instance; }}
 
+    #region Core
+    private DataManager _data = new DataManager();
+    private PoolManager _pool = new PoolManager();
+    private ResourceManager _resource = new ResourceManager();
+    private SceneManagerEx _scene = new SceneManagerEx();
+    private SoundManager _sound = new SoundManager();
+    private UIManager _ui = new UIManager();
+
+    // 외부에서 사용 시 Managers.Instance.Resource 등으로 메니저를 매니징 가능
+    public static DataManager Data { get { return Instance?._data; } }
+    public static PoolManager Pool { get { return Instance?._pool; } }
+    public static ResourceManager Resource { get { return Instance?._resource; } }
+    public static SceneManagerEx Scene { get { return Instance?._scene; } }
+    public static SoundManager Sound { get { return Instance?._sound; } }
+    public static UIManager UI { get { return Instance?._ui; } }
+    #endregion
+
     // 게임 생성 시 @Managers 라는 게임 오브젝트 생성 및 컴포넌트 할당
     public static void Init() {
         if (s_instance == null) {
