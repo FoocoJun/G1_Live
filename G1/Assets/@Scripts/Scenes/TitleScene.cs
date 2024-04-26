95af1bad6704fae78d2521b06e93c73f2b7e9d60
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2024-04-26 23:54:15 +0900

    create files and apply BaseScene class to other scene

--- G1/Assets/@Scripts/Scenes/TitleScene.cs
+++ G1/Assets/@Scripts/Scenes/TitleScene.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TitleScene : MonoBehaviour
+public class TitleScene : BaseScene
 {
     // Start is called before the first frame update
     void Start()
