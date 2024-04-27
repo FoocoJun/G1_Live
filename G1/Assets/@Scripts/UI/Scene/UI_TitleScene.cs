df0104a7f2e31cd53feba652344889cc86251259
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2024-04-27 15:48:48 +0900

    update UI_TitleScene

--- G1/Assets/@Scripts/UI/Scene/UI_TitleScene.cs
+++ G1/Assets/@Scripts/UI/Scene/UI_TitleScene.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using static Define;
 
-public class UI_TitleScene : MonoBehaviour
+public class UI_TitleScene : UI_Scene
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    // 0. 해당 씬에 속하는 각 타입에 따라 이넘 생성
+    enum GameObjects {
+        StartImage
     }
 
-    // Update is called once per frame
-    void Update()
+    enum Texts {
+        DisplayText
+    }
+
+    public override bool Init()
     {
-        
+        if (base.Init() == false) {
+            return false;
+        }
+
+        BindObjects(typeof(GameObjects));
+        BindTexts(typeof(Texts));
+
+        InitStartImage();
+        InitDisplayText();
+
+        StartLoadAssets();
+
+        return true;
+    }
+
+    void InitStartImage() {
+        // 호출 시 이넘으로 호출 가능
+        GetObject((int)GameObjects.StartImage).BindEvent((evt) => {
+            Debug.Log("Change Scene");
+            Managers.Scene.LoadScene(Define.EScene.GameScene);
+        });
+
+        GetObject((int)GameObjects.StartImage).gameObject.SetActive(false);
+    }
+
+    void InitDisplayText() {
+        GetText((int)Texts.DisplayText).text = $"";
+    }
+
+    void StartLoadAssets()
+    {
+        Managers.Resource.LoadAllAsync<Object>("PreLoad", (key, count, totalCount) => {
+            Debug.Log($"{key} {count}/{totalCount}");
+
+            if (count == totalCount) {
+                GetObject((int)GameObjects.StartImage).gameObject.SetActive(true);
+                GetText((int)Texts.DisplayText).text = $"Touch To Start";
+            }
+        });
     }
 }
