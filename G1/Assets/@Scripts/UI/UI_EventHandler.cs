47834e3c6f22b881da29498ad47c0c3b79cdc58b
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2024-04-27 14:27:48 +0900

    create UI Event handler

--- G1/Assets/@Scripts/UI/UI_EventHandler.cs
+++ G1/Assets/@Scripts/UI/UI_EventHandler.cs
@@ -1,19 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-// 클릭 시 호버 시 등 핸들러
-public class UI_EventHandler : MonoBehaviour
+// 클릭 시 호버 시 등 핸들러 필요에 따라 추가제거
+public class UI_EventHandler : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler, IDragHandler
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public event Action<PointerEventData> OnClickHandler = null;
+    public event Action<PointerEventData> OnPointerDownHandler = null;
+    public event Action<PointerEventData> OnPointerUpHandler = null;
+    public event Action<PointerEventData> OnDragHandler = null;
+
+    public void OnPointerClick(PointerEventData eventData) {
+        OnClickHandler?.Invoke(eventData);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void OnPointerDown(PointerEventData eventData) {
+        OnPointerDownHandler?.Invoke(eventData);
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        OnPointerUpHandler?.Invoke(eventData);
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        OnDragHandler?.Invoke(eventData);
     }
 }
