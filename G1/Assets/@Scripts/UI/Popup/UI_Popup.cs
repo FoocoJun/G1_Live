2063a0deb5b69ffbe41b036278b14d3a1465bcf5
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2024-04-27 15:06:25 +0900

    update UIManager and apply it to UI Scripts

--- G1/Assets/@Scripts/UI/Popup/UI_Popup.cs
+++ G1/Assets/@Scripts/UI/Popup/UI_Popup.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Popup은 타 UI와 다르게 순서 등 핸들링 필요
-public class UI_Popup : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class UI_Popup : UI_Base {
+    public override bool Init() {
+        if (base.Init() == false) {
+            return false;
+        }
         
+        Managers.UI.SetCanvas(gameObject, true);
+        return true;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public virtual void ClosePopupUI() {
+        Managers.UI.ClosePopupUI(this);
     }
 }
+
--- G1/Assets/@Scripts/UI/Scene/UI_Scene.cs
+++ G1/Assets/@Scripts/UI/Scene/UI_Scene.cs
@@ -3,17 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Scene에 고정되는 주요 UI들 핸들링
-public class UI_Scene : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class UI_Scene : UI_Base {
+    public override bool Init() {
+        if (base.Init() == false) {
+            return false;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Managers.UI.SetCanvas(gameObject, false);
+
+        return true;
     }
 }
