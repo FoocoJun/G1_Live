5f73a1fed7cd1f19c4f2a8df07881a8ce19e81b0
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2024-04-27 14:27:30 +0900

    create Finding Utils

--- G1/Assets/@Scripts/Utils/Util.cs
+++ G1/Assets/@Scripts/Utils/Util.cs
@@ -1,18 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Util : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class Util {
+    // 컴포넌트 호출 후 없으면 만들어서 뱉기
+    public static T GetOrAddComponent<T>(GameObject go) where T : UnityEngine.Component {
+        T component = go.GetComponent<T>();
+        if (component == null) {
+            component = go.AddComponent<T>();
+        }
+
+        return component;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public static GameObject FindChild(GameObject go, string name = null, bool recursive = false) {
+        Transform transform = FindChild<Transform>(go, name, recursive);
+        if (transform == null) {
+            return null;
+        }
+        return transform.gameObject;
+    }
+
+    // 자녀 컴포넌트 중 name이 같은 것 찾기.
+    public static T FindChild<T>(GameObject go, string name = null, bool recursive = false) where T : UnityEngine.Object {
+        if (go == null) {
+            return null;
+        }
+
+        if (recursive == false) {
+            for (int i = 0; i < go.transform.childCount; i++) {
+                Transform transform = go.transform.GetChild(i);
+                if (string.IsNullOrEmpty(name) || transform.name == name) {
+                    T component = transform.GetComponent<T>();
+                    if (component != null) {
+                        return component;
+                    }
+                }
+            }
+        } else {
+            foreach (T component in go.GetComponentsInChildren<T>()) {
+                if (string.IsNullOrEmpty(name) || component.name == name) {
+                    return component;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static T ParseEnum<T>(string value) {
+        return (T)Enum.Parse(typeof(T), value, true);
     }
 }
