07bf4c8e5fd1d0e8c9753913c514b3fcaf87f50c
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2024-05-05 21:37:26 +0900

    change name

--- G1/Assets/@Scripts/Controllers/Creature/Hero.cs
+++ G1/Assets/@Scripts/Controllers/Creature/Hero.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Hero : BaseObject
 {
     // Start is called before the first frame update
     void Start()
