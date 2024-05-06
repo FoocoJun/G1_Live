295a92738165df2f60e8323539a02163b2133e0a
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2024-05-06 16:52:14 +0900

    Update GameManager w/ MoveDir and JoystickState

--- G1/Assets/@Scripts/Managers/Contents/GameManager.cs
+++ G1/Assets/@Scripts/Managers/Contents/GameManager.cs
@@ -1,7 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Define;
 
 public class GameManager {
+    private Vector2 _moveDir;
+    private EJoystickState _joystickState;
 
+    #region Hero
+    public Vector2 MoveDir {
+        get { return _moveDir; }
+        set {
+            _moveDir = value;
+            OnMoveDirChanged?.Invoke(value);
+        }
+    }
+
+    public EJoystickState JoystickState {
+        get { return _joystickState; }
+        set {
+            _joystickState = value;
+            OnJoystickStateChanged?.Invoke(value);
+        }
+    }
+    #endregion
+
+    #region Action
+    public event Action<Vector2> OnMoveDirChanged;
+    public event Action<EJoystickState> OnJoystickStateChanged;
+    #endregion
 }
