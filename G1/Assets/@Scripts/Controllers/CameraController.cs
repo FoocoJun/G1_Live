9ca6496e6553b1ba4f8fac747fcf8705e0472b23
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2024-05-06 17:03:37 +0900

    create camera controller

--- G1/Assets/@Scripts/Controllers/CameraController.cs
+++ G1/Assets/@Scripts/Controllers/CameraController.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class CameraController : InitBase {
+    private BaseObject _target;
+    public BaseObject Target {
+        get { return _target; }
+        set {
+            _target = value;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public override bool Init() {
+        if (base.Init() == false) {
+            return false;
+        }
+
+        // 카메라 기본 배율
+        Camera.main.orthographicSize = 15.0f;
+
+        return true;
+    }
+
+    // 모든 update가 끝난 후 작동. 카메라 로직은 이쪽으로
+    void LateUpdate() {
+        if (Target == null) {
+            return;
+        }
+
+        Vector3 targetPosition = new Vector3(Target.CenterPosition.x, Target.CenterPosition.y, -10f);
+        transform.position = targetPosition;
     }
 }
