73377eba25b120dc12a942d843286d538c6ffee2
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2024-05-06 16:52:47 +0900

    apply Hero w/ Joystick Event

--- G1/Assets/@Scripts/Controllers/Creature/Hero.cs
+++ G1/Assets/@Scripts/Controllers/Creature/Hero.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 using static Define;
 
 public class Hero : Creature {
+    private Vector2 _moveDir = Vector2.zero;
     public override bool Init() {
         if (base.Init() == false) {
             return false;
@@ -13,6 +15,33 @@ public class Hero : Creature {
         CreatureState = ECreatureState.Idle;
         Speed = 5.0f;
 
+        Managers.Game.OnMoveDirChanged -= HandleOnMoveDirChanged;
+        Managers.Game.OnMoveDirChanged += HandleOnMoveDirChanged;
+        Managers.Game.OnJoystickStateChanged -= HandleOnJoystickStateChanged;
+        Managers.Game.OnJoystickStateChanged += HandleOnJoystickStateChanged;
+
         return true;
+    }
+
+    void Update() {
+        transform.Translate(_moveDir * Time.deltaTime * Speed);
+    }
+    private void HandleOnMoveDirChanged(Vector2 dir) {
+        _moveDir = dir;
+    }
+
+    private void HandleOnJoystickStateChanged(EJoystickState joystickState) {
+        switch (joystickState) {
+            case EJoystickState.PointerDown:
+                CreatureState = ECreatureState.Move;
+                break;
+            case EJoystickState.Drag:
+                break;
+            case EJoystickState.PointerUp:
+                CreatureState = ECreatureState.Idle;
+                break;
+            default:
+                break;
+        }
     }
 }
