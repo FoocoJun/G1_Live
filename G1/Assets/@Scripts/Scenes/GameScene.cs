816d06720b46c5744cfe9e025280356f733065b9
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2024-05-06 20:20:52 +0900

    add some monster

--- G1/Assets/@Scripts/Scenes/GameScene.cs
+++ G1/Assets/@Scripts/Scenes/GameScene.cs
@@ -17,7 +17,11 @@ public class GameScene : BaseScene {
         // TODO
 
         // 캐릭터 생성
-        Hero hero = Managers.Object.Spawn<Hero>(Vector3.zero);
+        Vector3 heroTmpStartPosition = new Vector3Int(-10, -5, 0);
+        Hero hero = Managers.Object.Spawn<Hero>(heroTmpStartPosition);
+
+        Vector3 monsterTmpStartPosition = new Vector3Int(0, 1, 0);
+        Monster monster = Managers.Object.Spawn<Monster>(monsterTmpStartPosition);
 
         // 카메라 셋팅
         CameraController camera = Camera.main.GetOrAddComponent<CameraController>();
